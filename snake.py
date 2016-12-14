6d5048141179c15359d50d94cb73428326631afc
Tramscan <kidscline02@gmail.com>
2016-12-14 09:56:28 -0500

    updated code
    will add desc later

--- snake.py
+++ snake.py
@@ -4,33 +4,31 @@ Created on Tue Nov  8 10:23:46 2016
 
 @author: lab301-user28
 """
-import os
-#import selenium, os, urllib
+
+import selenium, os, urllib
 from selenium import webdriver
 import win32com.client as comctl
 from random import randint
 import time
 import random
-#import lxml
+import lxml
 import math
-#from lxml import html
-#from urllib import request
-#import BeautifulSoup
-#from bs4 import BeautifulSoup
-#url="http://patorjk.com/games/snake/"
-#page=urllib2.urlopen(url)
-#soup=BeautifulSoup(page)
+from lxml import html
+from urllib import request
+
+from bs4 import BeautifulSoup
 
 chromedriver="C:/Python34/Scripts/chromedriver"
 os.environ["webdriver.chrome.driver"] = chromedriver
 
 wsh = comctl.Dispatch("WScript.Shell")
-wsh.AppActivate("http://www.cs.ucc.ie/~dgb/courses/wd2/labs/lab6/snake.html")
+wsh.AppActivate("http://www.me.umn.edu/~dockt036/snake.html")
 
 
 driver = webdriver.Chrome(executable_path='C:/Python34/Scripts/chromedriver')
-driver.get('http://www.cs.ucc.ie/~dgb/courses/wd2/labs/lab6/snake.html')
+driver.get('http://www.me.umn.edu/~dockt036/snake.html')
 hidden_element = driver.find_element_by_id('sbTryAgain0')
+element=driver.find_element_by_class_name('snake-panel-component')
 def createIndividual(length,mintim, maxtim): # creates array of arrays (individual = [[wait time(ex. float 0.3), wait time, etc][direction (ex. int 1, 1 corresponds to right), directon,][fitness of individual]])
     return [[random.uniform(mintim,maxtim) for x in range(length)],[randint(0,3) for x in range(length)],[]]
     
@@ -54,20 +52,20 @@ def run(ind):#runs through the movement code (2 arrays, one for wait time, one f
         print(changeInt(ind[1][x]))#prints what keys are being pressed
     
 def indfit(time): #finds individual's fitness
-    #tree=lxml.html.fromstring(str(page))
-    #points=response.read(tree.xpath('//div[@id="game-area"]/div[@class="snake-panel-component"]/text()'))
-    #print(points)
+    tree=lxml.html.fromstring(str(page))
+    points=tree.xpath('//div[@id="game-area"]/div[@class="snake-panel-component"]/text()')
+    print(points)
     #points=int(input("how many points did the individual get: "))#right now this code above is supposed to get the value in the bottom left hand corner for length. The school's firewall blocks the website, so it returns nothing.
-    """
+    
     for i in points:
         if(points[i-1]==" " and points[i-2]==":"):
             pts=points[8:]
             return math.pow(time,pts)
             break
-    """
-    pts=1 #temporary
+    
+    #pts=1 #temporary
     #return (pts/time)
-    return math.pow(time,pts)#if the snake gets a point, the fitness goes up exponentially so it can favor code that can get the most amount of points
+    #return math.pow(time,pts)#if the snake gets a point, the fitness goes up exponentially so it can favor code that can get the most amount of points
 
 def createPopulation(size, length, mintim, maxtim): #creates a population of individuals (array of array of arrays)
     population=[]
@@ -128,6 +126,10 @@ def evolve(pop,mutrate,killpercent,sammin,sammax):#evolves population
                 mixtraitsavg(pop[len(pop[0])-index1][1],pop[len(pop[0])-(index1+1)][1])
 
 index8=0
+url="http://www.me.umn.edu/~dockt036/snake.html"
+page=urllib.request.urlopen(url)
+with open('page.txt', 'w') as f:
+        f.write(element.text)
 while(index8<=50):#runs through 50 generations
     x=createPopulation(30,30,0,2)#creates population of size 30, each individual has 30 moves with a wait time between 0 and 2 seconds.
     cyclePop(x)#runs population
